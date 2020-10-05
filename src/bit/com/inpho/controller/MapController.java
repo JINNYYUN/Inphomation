f12a40e8d14ff479fc49bf6bc1cd6047ce76023d
YUNJIMYEONG <yjim3440@gmail.com>
2020-10-05 14:19:40 +0900

    [add] MapController 생성

--- src/bit/com/inpho/controller/MapController.java
+++ src/bit/com/inpho/controller/MapController.java
@@ -1,8 +1,20 @@
 package bit.com.inpho.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import bit.com.inpho.service.MapService;
 
 @Controller
 public class MapController {
-
+	@Autowired
+	MapService mapService;
+	
+	@RequestMapping(value = "map.do", method = RequestMethod.GET)
+	public String showMap() {
+		System.out.println("MapController showMap()");		
+		return "map.tiles";
+	}
 }
