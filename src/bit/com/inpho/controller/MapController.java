c8932b3e2804d303d2e64d0838d5d492352257e5
YUNJIMYEONG <yjim3440@gmail.com>
2020-10-08 16:29:45 +0900

    [edit] 불필요한 import 제거

--- src/bit/com/inpho/controller/MapController.java
+++ src/bit/com/inpho/controller/MapController.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import bit.com.inpho.dto.LocationDTO;
-import bit.com.inpho.dto.MapInfoDTO;
 import bit.com.inpho.service.MapService;
 
 @Controller
