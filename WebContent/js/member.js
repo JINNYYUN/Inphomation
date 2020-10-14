1c617acdd0458304dd1b51417492b4f370226675
donsonioc2010 <whddnjs822@gmail.com>
2020-10-14 12:00:42 +0900

    [add] 유효성검사 / 비밀번호 아이콘 클릭시에 비밀번호 보이기

--- WebContent/js/member.js
+++ WebContent/js/member.js
@@ -1,4 +1,47 @@
 
+//로그인버튼 유효성검사
+function submitLogin(){
+	let id=$("#login-id")
+	if(!isEmail(id.val())){
+		return id.focus()
+	}
+	let pw=$("#login-pw")
+	if(!isPassword(pw.val())){
+		return pw.focus()
+	}
+	$("#modal-form").submit()
+}
+
+
+//회원가입 유효성검사
+function submitRegi(){
+	let id=$("#regi-id")
+	if(!isEmail(id.val())){
+		return id.focus()
+	}
+	let pw=$("#regi-pw")
+	if(!isPassword(pw.val())){
+		return pw.focus()
+	}
+	let name=$('#regi-nickname')
+	if(name.val().trim()==''){
+		return name.focus()
+	}
+	
+
+	$("#modal-form").submit()
+	
+}
+//비밀번호 8-10확인
+function isPassword(asValue) {
+	var regExp = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/
+	return regExp.test(asValue) // 형식에 맞는 경우 true 리턴
+}
+//이메일 양식확인
+function isEmail(asValue) {
+	var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+	return regExp.test(asValue) // 형식에 맞는 경우 true 리턴	
+}
 
 //카카오로그인
 function loginWithKakao() {
@@ -17,6 +60,24 @@ function loginWithKakao() {
     })
   }
   
+//naver로그인
+function loginWithNaver(){
+	$.ajax({
+		url:"getNaverLink",
+		type:"get",
+		success:function(url){
+			let name="naverLogin"
+			let option = "width=600,height=500,top=400,left=400,location=no"
+			doPopup(url,name,option)
+		},
+		error:function(){
+			alert('문제발생')
+		}
+	})
+}
+function doPopup(url,name,option){
+	window.open(url, name, option)
+}
   /*
   카카오 데이터
 {access_token: "vVI2xZGn7Xefdr3GSiamwYlKbKS0qbzW5puCgQopyV8AAAF1GC47PA", token_type: "bearer", refresh_token: "nm9t-rQiwRrr4OcD2BDCQm6aiNXyJAow35yvGwopyV8AAAF1GC47Og", expires_in: 7199, scope: "account_email profile", …}
